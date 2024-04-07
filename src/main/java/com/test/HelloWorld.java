09e284e026fd24d7a28c10bbec7a93ff678d57e5
Fellipe Parra <33fparra@gmail.com>
2024-04-07 01:43:19 -0400

    First commit Eva 1 de testeo

---
+++ src/main/java/com/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println(getSaludo());
+
+	}
+	public static String getSaludo() {
+		return "Test de aplicaicon en Eclipse Eva1";
+		}
+
+}
